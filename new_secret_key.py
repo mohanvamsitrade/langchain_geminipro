7dec611d639095076b27cbafac9da82947180a2b
mohanvamsitrade <39032710+mohanvamsitrade@users.noreply.github.com>
2024-02-13 01:01:27 +0530

    Update new_secret_key.py

--- new_secret_key.py
+++ new_secret_key.py
@@ -1,10 +1,3 @@
 
 
 
-moni_openapi_key1 = "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"
-
-huggingface_key1 = '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'
-
-google_api_key = "AIzaSyDfNvott3AKGPiHn203SU774nd1l86GjPY"
-
-google_palm_api_key = "AIzaSyDWpvF_jwclH7z5ohGEyntNoyjq8CFPxUg"
